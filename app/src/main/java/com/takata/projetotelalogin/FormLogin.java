ca2c0e200039928a70df2af2c05057b78c7392e2
Felipe Takata de Alvarenga <felipetakatadealvarenga@gmail.com>
2022-08-30 11:37:10 -0300

    Feat FormCadastro

--- app/src/main/java/com/takata/projetotelalogin/FormLogin.java
+++ app/src/main/java/com/takata/projetotelalogin/FormLogin.java
@@ -3,8 +3,11 @@ package com.takata.projetotelalogin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,9 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
